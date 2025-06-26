028b41d2a24306dedb970e3acaac3e324e4e27a9
Inkee Jung <inkeej@bu.edu>
2025-06-25 22:07:34 -0700

    Suppress dtype warnings, fix merge key bug, refactor phase to include lat/lon and rename function

--- 01_ntsb_table_join.py
+++ 01_ntsb_table_join.py
@@ -14,7 +14,9 @@ def read_engines(filename):
     engines = pd.read_csv(filename,usecols=['ev_id',
                                             'Aircraft_Key',
                                             'eng_type',
-                                            'eng_no']) # Extract relevant columns
+                                            'eng_no'] # Extract relevant columns
+                          , dtype = {'ev_id': str} # Avoid dtype warning
+                          )
 
     engines = engines[engines['eng_no']==1].drop(columns='eng_no') # Record information about first engine only
     engines['event_key'] = engines['ev_id'].astype(str) + '_' + engines['Aircraft_Key'].astype(str) # Create event_key
@@ -45,8 +47,8 @@ def read_aircraft(filename):
                                             'rwy_width',
                                             'acft_year',
                                             'fuel_on_board',
-                                            'unmanned']    
-                                            ) # Extract Relevant Columns
+                                            'unmanned']     # Extract Relevant Columns
+                                            , dtype = {'ev_id': str}) # Avoid dtype warning
     aircraft['event_key'] = aircraft['ev_id'].astype(str) + '_' + aircraft['Aircraft_Key'].astype(str) # Create event key 
     
     return aircraft
@@ -56,7 +58,8 @@ def read_injuries(filename):
     '''Read injury data
     The .csv file lists separate injury counts for passengers, crew, etc. so this requires some processing 
     '''
-    inj_df = pd.read_csv(filename)
+    inj_df = pd.read_csv(filename
+                         , dtype = {'ev_id': str}) # Avoid dtype warning
 
     ## Remove NaN values to ensure valid summation when grouping by 'event_key'
     inj_df = inj_df[~inj_df['inj_person_count'].isnull()]
@@ -100,43 +103,50 @@ def read_injuries(filename):
     return injuries
 
 
-def read_phase(filename):
-    '''Read phase data'''
-    phase = pd.read_csv(filename, usecols=['NtsbNo','BroadPhaseofFlight'])
+def read_from_carol(filename):
+    '''Read phase, longitude, and latitude data from CAROL, NTSB query tool.'''
+    carol_data = pd.read_csv(filename, usecols=['NtsbNo',
+                                           'BroadPhaseofFlight', 
+                                           'Latitude',
+                                           'Longitude'])
 
     # Rename to match corresponding column name in events
-    phase.rename({'NtsbNo':'ntsb_no'}, axis=1, inplace=True)
-
-    return phase
+    carol_data.rename({'NtsbNo':'ntsb_no', 'Latitude':'latitude', 'Longitude':'longitude'}, axis=1, inplace=True)
+    return carol_data
 
 
 def read_events(events_filename):
-    events = pd.read_csv(events_filename,usecols=['ev_id',
-                                                            'ntsb_no',
-                                                            'ev_country',
-                                                            'ev_type',
-                                                            'ev_year',
-                                                            'ev_month',
-                                                            'ev_date',
-                                                            'latitude',
-                                                            'longitude',
-                                                            'apt_dist',
-                                                            'altimeter',
-                                                            'ev_time',
-                                                            'light_cond',
-                                                            'wind_vel_kts',
-                                                            'gust_kts',
-                                                            'ev_highest_injury',
-                                                            'inj_tot_f',
-                                                            'inj_tot_m',
-                                                            'inj_tot_n',
-                                                            'inj_tot_s',
-                                                            'inj_tot_t',
-                                                            'on_ground_collision',
-                                                            'inj_f_grnd',
-                                                            'inj_m_grnd',
-                                                            'inj_s_grnd',
-                                                            ]) # Extract relevant columns
+    '''
+    Read event table.
+    '''
+    events = pd.read_csv(events_filename,
+                         usecols=['ev_id',
+                                'ntsb_no',
+                                'ev_country',
+                                'ev_type', 
+                                'ev_year',
+                                'ev_month',
+                                'ev_date',
+                                # 'latitude',
+                                # 'longitude', ## latitude and longitude are obtained from CAROL NTSB query
+                                'apt_dist',
+                                'altimeter',
+                                'ev_time',
+                                'light_cond',
+                                'wind_vel_kts',
+                                'gust_kts',
+                                'ev_highest_injury',
+                                'inj_tot_f',
+                                'inj_tot_m',
+                                'inj_tot_n',
+                                'inj_tot_s',
+                                'inj_tot_t',
+                                'on_ground_collision',
+                                'inj_f_grnd',
+                                'inj_m_grnd',
+                                'inj_s_grnd',
+                                ] # Extract relevant columns
+                         , dtype = {'ev_id': str}) # Avoid dtype warning
 
     # Rename injury columns to distinguish event-level injury counts from aircraft-level injury counts
     new_names = {
@@ -147,24 +157,25 @@ def read_events(events_filename):
         'inj_tot_t': 'ev_inj_tot_t',
     }
     events.rename(columns=new_names, inplace=True)
-
     return events.copy()
 
 
-def merge_data(engines, aircraft, injuries, events, phase):
-    #TODO finish cleaning this up
-    '''DOCSTRING'''
+def merge_data(engines, aircraft, injuries, events, carol_data):
+    """
+    Merge NTSB aircraft-level tables (engines, aircraft, injuries) and data from CAROL NTSB query (Latitude, Longitude, phase) with event-level data into a unified DataFrame.
+    """
 
     tables = pd.merge(engines,aircraft,on=['event_key','ev_id','Aircraft_Key'],how='left') # Join on all 3 to avoid _x and _y duplicate columns.  Also ensures correct specification.
     tables = pd.merge(tables,injuries,on=['event_key','ev_id','Aircraft_Key'],how='left') # Join "injuries" data as well.
     tables['Aircraft_ID'] = tables.groupby('ev_id').cumcount() + 1 # Some events start at 2 - this line creates a new Aircraft Key that is uniform across all coding schemes.
+    
     tables['event_key'] = tables['ev_id'].astype(str) + '_' + tables['Aircraft_ID'].astype(str) # Resets the "event_key" variable to match our adjusted aircraft ID
 
     aircraft_counts = pd.DataFrame(tables.groupby('ev_id')['Aircraft_ID'].count()).reset_index() # Counts how many unique values of "Aircraft_ID" per event - need this to tell "events" set where to duplicate
     aircraft_counts.rename(columns={'Aircraft_ID':'aircraft_count'},inplace=True)
     aircraft_counts['aircraft_count'] = aircraft_counts['aircraft_count'].fillna(1) # Fill in missing - if no aircraft info, assume 1 (true for most)
 
-    data = pd.merge(events, phase, how='left', on=['ntsb_no'])
+    data = pd.merge(events, carol_data, how='left', on=['ntsb_no'])
 
     df = pd.merge(data,aircraft_counts,on='ev_id',how='left') # Concatenates "aircraft count" var to dataset, will indicate how many replicate rows to generate
     df['aircraft_count'] = df['aircraft_count'].fillna(1) # Some events were not in any of the 3 supplemental sets - set aircraft count as 1 for these
@@ -173,6 +184,7 @@ def merge_data(engines, aircraft, injuries, events, phase):
 
     df_repeated['Aircraft_ID'] = df_repeated.groupby('ev_id').cumcount() + 1 # Re-creates aircraft ID in the event data so we can join the individual aircraft from the tables dataset
     df_repeated['event_key'] = df_repeated['ev_id'].astype(str) + '_' + df_repeated['Aircraft_ID'].astype(str) # Re-creates "event_key" for same reason.
+    
 
     merged = df_repeated.merge(tables, on=['event_key','ev_id','Aircraft_ID'], how='left') # Joins "event" dataset with supplemental sets.
 
@@ -180,7 +192,24 @@ def merge_data(engines, aircraft, injuries, events, phase):
 
 
 def stratified_group_split(data, strat_col, group_col):
+    """
+        Perform a stratified group split to divide data into training, validation, and test sets. The distribution of the stratification variable (e.g. strat_col='damage') is preserved across splits.
+        
+        The function uses `StratifiedGroupKFold` with 5 folds, treating the first fold as the test set and the second fold as the validation set. The resulting split consists of 60% training, 20% validation, and 20% test data.
+        
+    Parameters:
+    -----
+    data : pd.DataFrame
+    The input dataset to be split.
 
+    strat_col : str
+        Column name on which to stratify the data (e.g., 'damage').
+
+    group_col : str
+        Column name used to group data points (e.g., 'ev_id') so that all rows
+        within the same group are kept together in a single split.
+    -----
+    """
     # Validation and test will both be 20% of data, hence 5 splits
     strat_kfold = StratifiedGroupKFold(n_splits = 5, shuffle=True, random_state=412)  
 
@@ -207,12 +236,12 @@ if __name__ == '__main__':
     engines = read_engines('data/ntsb_raw/ntsb_engines.csv')
     aircraft = read_aircraft('data/ntsb_raw/ntsb_aircraft.csv')
     injuries = read_injuries('data/ntsb_raw/ntsb_injuries.csv')
-    phase = read_phase('data/ntsb_raw/ntsb_from_carol.csv')
+    carol_data = read_from_carol('data/ntsb_raw/ntsb_from_carol.csv')
     events = read_events('data/ntsb_raw/ntsb_events.csv')
-
-    ### 2. Merge data into a single DataFrame
-    merged = merge_data(engines, aircraft, injuries, phase, events)
     
+    ### 2. Merge data into a single DataFrame
+    merged = merge_data(engines, aircraft, injuries, events,carol_data)
+
     ### 3. Restrict data to the scope of our problem:
     #      - Accidents
     #      - In the USA
