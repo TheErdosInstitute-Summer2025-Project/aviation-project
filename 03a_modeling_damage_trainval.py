d21ed690c960117e4170d182a88795f80e76147c
Inkee Jung <inkeej@bu.edu>
2025-06-26 23:41:52 -0700

    modeling damage script part 1 and part 2; presentation file separated; images generated

--- 03a_modeling_damage_trainval.py
+++ 03a_modeling_damage_trainval.py
@@ -1,12 +1,12 @@
-## Modeling: Damage
+## Modeling Damage, Part 1
 
 # This script imports the NTSB training and validation sets,
 # and iteratively tests the performance of various regression-based learners
 # while grid searching over a range of hyperparameters for each, comparing
 # the performance of all learners to each other and to a naive prediction
 # which takes all 'SUBS'. After establishing the 
-# best performing learner, feature importances are printed and the 
-# model is applied to the test dataset.
+# best performing learner, classification and confusion matrix are printed and the 
+# model is applied to the test dataset in part 2.
 
 # Load Libraries
 
@@ -14,11 +14,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pickle as pkl
+import argparse
+import json
+import os
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier, ExtraTreesClassifier
 from sklearn.metrics import f1_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
 from xgboost import XGBClassifier
+seed = 51841519 # Set random seed - when re-fitting in 3b, ensures reproducibility
 
 # Read in data, prepare for evaluation
 ## NOTE: The train/validation split has already been conducted at an earlier stage.
@@ -36,25 +41,25 @@ def load_data():
 
 
 models = {
-        "randomforest": (RandomForestClassifier(), {
+        "randomforest": (RandomForestClassifier(random_state=seed), {
     'n_estimators': [100, 200],
     'max_depth': [None, 5, 10, 20],
     'min_samples_split': [2, 5, 10],
     'max_features': ['sqrt', 'log2', None]
 }),
-        "histgrad": (HistGradientBoostingClassifier(), {
+        "histgrad": (HistGradientBoostingClassifier(random_state=seed), {
     'learning_rate': [0.01, 0.05, 0.1, 0.5],
     'max_iter': [100, 200, 500],
     'max_leaf_nodes': [3,9,15,31],
     'min_samples_leaf': [10, 20, 30]
 }),
-        "extrees": (ExtraTreesClassifier(), {
+        "extrees": (ExtraTreesClassifier(random_state=seed), {
     'n_estimators': [100, 200, 500],
     'max_depth': [2, 5, 10, 20, 30],
     'min_samples_split': [2, 5, 10, 20],
     'max_features': ['sqrt', 'log2', None]
 }),
-        "xgboost": (XGBClassifier(), {
+        "xgboost": (XGBClassifier(random_state=seed), {
     'n_estimators': [20, 100, 200, 500],
     'max_depth': [3, 6, 10],
     'learning_rate': [0.01, 0.1, 0.3],
@@ -66,18 +71,7 @@ models = {
 PRESET_PARAMS = {k: v[1] for k, v in models.items()}
 
 
-# Initialize performance dataframe
-performances = pd.DataFrame(columns=['learner','hyperparams','target',
-                                     'train_f1_macro','val_f1_macro'])
 
-# Initialize feature importance dataframe dict
-feature_importance_df_dict = {}
-
-# Initialize classification reports dict
-classification_report_dict = {}
-
-# Initialize confusion matrix dict
-confusion_matrix_dict = {}
 
 
 
@@ -85,7 +79,7 @@ confusion_matrix_dict = {}
 def grid_search(model, label, 
                 X_train, X_val, 
                 y_train_encoded, y_val_encoded, 
-                features, param_grid=None):
+                param_grid=None):
     if USE_PRESET_PARAMS and param_grid is None:
         param_grid = PRESET_PARAMS[label]
 
@@ -93,7 +87,8 @@ def grid_search(model, label,
         model,
         param_grid,
         scoring='f1_macro',
-        cv=5
+        cv=5,
+        verbose=1
     )
     grid.fit(X_train, y_train_encoded)
     best_mod = grid.best_estimator_
@@ -102,13 +97,7 @@ def grid_search(model, label,
     y_train_pred = best_mod.predict(X_train)
     y_val_pred = best_mod.predict(X_val)
 
-    if label != 'xgboost' and label!='histgrad':
-        feature_importance_df = pd.DataFrame({
-            'Feature': features,
-            'Importance': best_mod.feature_importances_
-        }).sort_values(by="Importance", ascending=False).reset_index(drop=True)
-        feature_importance_df_dict[label] = feature_importance_df
-
+    
     f1_train = f1_score(y_train_encoded, y_train_pred, average='macro')
     f1_val = f1_score(y_val_encoded, y_val_pred, average='macro')
 
@@ -117,10 +106,13 @@ def grid_search(model, label,
         str(best_params),
         'damage',
         f1_train,
-        f1_val
+        f1_val,
+        grid.best_score_
     ]
-
-    classification_report_dict[label] = classification_report(y_val_encoded, y_val_pred)
+    classification_report_dict[label] = {
+        "report_dict": classification_report(y_val_encoded, y_val_pred, output_dict=True),
+        "report_str": classification_report(y_val_encoded, y_val_pred)
+    }
     confusion_matrix_dict[label] = confusion_matrix(y_val_encoded, y_val_pred)
 
     return best_mod
@@ -159,22 +151,23 @@ def main(use_preset=True, user_params={}):
     label_map = {'MINR': 0, 'SUBS': 1, 'DEST': 2}
     y_train_encoded = y_train.map(label_map)
     y_val_encoded = y_val.map(label_map)
-
-    global performances, feature_importance_df_dict, classification_report_dict, confusion_matrix_dict
-    performances = pd.DataFrame(columns=['learner', 'hyperparams', 'target', 'train_f1_macro', 'val_f1_macro'])
-    feature_importance_df_dict = {}
+    
+    global performances, classification_report_dict, confusion_matrix_dict
+    performances = pd.DataFrame(columns=['learner', 'hyperparams', 'target', 'train_f1_macro', 'val_f1_macro','cv_f1_macro'])
     classification_report_dict = {}
     confusion_matrix_dict = {}
 
     
     for label, (model, _) in models.items():
-        grid_search(model, label, X_train, X_val, y_train_encoded, y_val_encoded, features,
-                    param_grid=None if USE_PRESET_PARAMS else user_params)
+        grid_search(model, label, X_train, X_val, y_train_encoded, y_val_encoded,
+                    param_grid = None if USE_PRESET_PARAMS else user_params)
 
     y_dumb_val = np.full_like(y_val_encoded, 1)
+    dumb_score = f1_score(y_val_encoded, y_dumb_val, average='macro')
     performances.loc[len(performances)] = ["Naive", 'None', target,
-                                           f1_score(y_train_encoded, np.full_like(y_train_encoded, 1), average='macro'),
-                                           f1_score(y_val_encoded, y_dumb_val, average='macro')]
+                                           dumb_score,
+                                           dumb_score,
+                                           dumb_score]
 
     ############################################################
     ##             Model Comparison (w/ Naive Learner)        ##
@@ -183,126 +176,41 @@ def main(use_preset=True, user_params={}):
     print(performances)
 
     for label, cm in confusion_matrix_dict.items():
-        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=list(label_map.keys()))
-        disp.plot(cmap='Blues', xticks_rotation=45)
-        disp.ax_.set_title(f"Confusion Matrix of {label}")
-        disp.figure_.tight_layout()
-        if label in feature_importance_df_dict:
-            print(f"\nFeature importances of {label}:")
-            print(feature_importance_df_dict[label].head(10))
         print(f"\nClassification Report of {label}:")
         print(classification_report_dict[label])
+        print(f"\nConfusion matrix of {label}:")
         print(label, cm)
 
 if __name__ == "__main__":
-    import argparse
-    import json
 
-    parser = argparse.ArgumentParser(description="Run damage model with or without grid search")
-    parser.add_argument('--grid', action='store_true', help="Run full grid search (default is preset params)")
-    parser.add_argument('--params', type=str, default=None,
-                        help='''Optional JSON string of user-defined hyperparameters.
-Example:
---params '{"randomforest": {"n_estimators": [100, 200], "max_depth": [30]},
-           "xgboost": {"n_estimators": [100], "learning_rate": [0.1]},
-           "histgrad": {"n_estimators": [100], "max_iter": [200]},
-           "extrees": {"n_estimators": [100], "max_depth": [10]}}'
-''')
 
-    args = parser.parse_args()
+#     parser = argparse.ArgumentParser(description="Run damage model with or without grid search")
+#     parser.add_argument('--grid', action='store_true', help="Run full grid search (default is preset params)")
+#     parser.add_argument('--params', type=str, default=None,
+#                         help='''Optional JSON string of user-defined hyperparameters.
+# Example:
+# --params '{"randomforest": {"n_estimators": [100, 200], "max_depth": [30]},
+#            "xgboost": {"n_estimators": [100], "learning_rate": [0.1]},
+#            "histgrad": {"n_estimators": [100], "max_iter": [200]},
+#            "extrees": {"n_estimators": [100], "max_depth": [10]}}'
+# ''')
 
-    if args.params:
-        user_params = json.loads(args.params)
-    else:
-        user_params = {}
+#     args = parser.parse_args()
 
-    main(use_preset=not args.grid, user_params=user_params)
+#     if args.params:
+#         user_params = json.loads(args.params)
+#     else:
+#         user_params = {}
+
+    main()
     
-## The following full parameter grids were used to determine the default settings:
-
-# # Random Forest
-# rf_param_grid = {
-#     'n_estimators': [100, 200, 500],
-#     'max_depth': [None, 5, 10, 20, 30],
-#     'min_samples_split': [2, 5, 10, 20],
-#     'max_features': ['sqrt', 'log2', None]
-# }
-
-# # Histogram Gradient Boosting
-# hg_param_grid = {
-#     'learning_rate': [0.01, 0.05, 0.1, 0.5],
-#     'max_iter': [100, 200, 500],
-#     'max_leaf_nodes': [15, 31, 63],
-#     'min_samples_leaf': [20, 50, 100]
-# }
-
-# # Extra Trees
-# et_param_grid = {
-#     'n_estimators': [100, 200, 500],
-#     'max_depth': [None, 5, 10, 20, 30],
-#     'min_samples_split': [2, 5, 10, 20],
-#     'max_features': ['sqrt', 'log2', None]
-# }
-
-# # XGBoost
-# xgb_param_grid = {
-#     'n_estimators': [20, 100, 200, 500, 1000],
-#     'max_depth': [3, 6, 10],
-#     'learning_rate': [0.01, 0.1, 0.3],
-#     'subsample': [0.6, 0.8, 1.0],
-#     'colsample_bytree': [0.6, 0.8, 1.0]
-# }
-
 
 ############################################################
-##             Performance Data Export                    ##
+##             Performance and results Export             ##
 ############################################################
 
-performances.to_csv('data/classification_performances.csv',index=False)
-
-# Filter only for MSE columns
-df_filtered = performances.copy()
-
-f1_plot_df = df_filtered.melt(
-    id_vars=['learner', 'target'],
-    value_vars=['train_f1_macro', 'val_f1_macro'],
-    var_name='metric',
-    value_name='score'
-)
-
-
-# Extract 'Train' and "Validation"
-f1_plot_df['dataset'] = f1_plot_df['metric'].apply(lambda x: 'Train' if 'train' in x else 'Validation')
-
-# Sort learners by average score across all targets/datasets
-sorted_order = (
-    f1_plot_df.groupby('learner')['score']
-    .mean()
-    .sort_values()
-    .index
-    .tolist()
-)
-
-# Plot
-g = sns.catplot(
-    data=f1_plot_df,
-    kind='bar',
-    y='learner',
-    x='score',
-    hue='dataset',
-    col='target',
-    palette='Blues_d',
-    height=5,
-    aspect=1.4,
-    sharey=True,
-    order=sorted_order
-)
-
-
-g.set_titles("Target: {col_name}")
-g.set_axis_labels("f1_score", "Model")
-g.fig.suptitle("Training vs Validation f1_score by Target Variable", y=1.08)
-
-
-plt.tight_layout()
-g.savefig('img/damage_learners_scores.png')
+    performances.to_csv('data/model_performance/classification_performances_trainval.csv',index=False)
+    with open('data/model_performance/classification_report_dict_val.pkl', 'wb') as f:
+        pkl.dump(classification_report_dict, f)
+    with open('data/model_performance/classification_confusion_matrix_dict_val.pkl', 'wb') as f:
+        pkl.dump(confusion_matrix_dict, f)
---
+++ 03b_modeling_damage_test.py
@@ -0,0 +1,227 @@
+## Modeling Damage, Part 2
+
+# This script examines the performances of the models from Part 1,
+# extracts the models with the best performance,
+# fits it to the training set one last time, and then predicts the proportions in the 
+# test set.  Finally, performance of test predictions, classification report, confusion matrix and feature importances are 
+# displayed or saved.
+
+# Note: These modeling scripts are separated in order to avoid re-running
+
+# Load Libraries
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle as pkl
+import ast
+
+from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier, ExtraTreesClassifier
+from sklearn.metrics import f1_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
+from xgboost import XGBClassifier
+seed = 51841519 # Set random seed - when re-fitting, ensures reproducibility
+label_map = {'MINR':0, 'SUBS' :1, 'DEST' :2}
+
+############################################################
+##             Checking Model Performances                ##
+############################################################
+
+## load performance data obtained from the part 1
+performances = pd.read_csv('data/model_performance/classification_performances_trainval.csv')
+with open('data/model_performance/classification_report_dict_val.pkl', 'rb') as f:
+    classification_report_dict = pkl.load(f)
+with open('data/model_performance/classification_confusion_matrix_dict_val.pkl', 'rb') as f:
+    classification_confusion_matrix_dict = pkl.load(f)
+
+df = performances.copy()
+best_row = performances[performances['val_f1_macro'] == performances['val_f1_macro'].max()]
+print('best model performance:\n',best_row) # Best model
+
+
+learner = best_row['learner'].values[0] # string format of classifier type
+
+
+
+# Performance Plot
+
+
+
+f1_plot_df = df.melt(
+    id_vars=['learner', 'target'],
+    value_vars=['train_f1_macro', 'val_f1_macro'],
+    var_name='metric',
+    value_name='score'
+)
+
+
+# Extract 'Train' and "Validation"
+f1_plot_df['dataset'] = f1_plot_df['metric'].apply(lambda x: 'Train' if 'train' in x else 'Validation')
+
+# Sort learners by average score across all datasets
+sorted_order = (
+    f1_plot_df.groupby('learner')['score']
+    .mean()
+    .sort_values(ascending=False)
+    .index
+    .tolist()
+)
+
+g = sns.catplot(
+    data=f1_plot_df,
+    kind='bar',
+    y='learner',
+    x='score',
+    hue='dataset',
+    col='target',
+    palette='Blues_d',
+    height=5,
+    aspect=1.4,
+    sharey=False,
+    order=sorted_order
+)
+
+g.set_titles("Target: {col_name}")
+g.set_axis_labels("f1_score", "Model")
+g.fig.suptitle("Training vs Validation f1_score by Target Variable", y=1.08)
+g.set_xticklabels(rotation=45)
+
+plt.tight_layout()
+g.savefig('img/validation_damage_learners_scores_trainval.png', bbox_inches='tight')
+    
+# Classification Report (includes precision, recall, f1 per class)
+print(f"\nClassification Report of {learner} on validation dataset:")
+print(classification_report_dict[learner]["report_str"])
+
+## Confusion Matrix
+cm = classification_confusion_matrix_dict[learner]
+# label_map = {'MINR':0, 'SUBS' :1, 'DEST' :2}
+labels_map = {v: k for k, v in label_map.items()}
+labels = [labels_map[i] for i in sorted(labels_map)]
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+disp.plot(cmap='Blues', xticks_rotation=45)
+
+## Export confusion matrix
+disp.ax_.set_title(f"Confusion Matrix of {learner} on validation dataset")
+disp.ax_.set_xlabel("Predicted Label")
+disp.ax_.set_ylabel("True Label")
+disp.figure_.tight_layout()
+plt.savefig('img/classification_confusion_matrix_val.png', dpi=300, bbox_inches='tight')  # Save as PNG
+
+
+
+
+
+############################################################
+##                 Predicting Test Set                    ##
+############################################################
+
+
+train = pd.read_csv('data/ntsb_processed/ntsb_train_cleaned.csv')
+test = pd.read_csv('data/ntsb_processed/ntsb_test_cleaned.csv')
+target = 'damage'
+train = train[train[target] != 'UNK']
+test = test[test[target] != 'UNK']
+
+features = ['latitude','longitude','apt_dist','gust_kts','altimeter','aircraft_count',
+            'num_eng','days_since_insp','light_cond_DAYL','light_cond_DUSK','light_cond_NDRK',
+            'light_cond_NITE','light_cond_other/unknown','BroadPhaseofFlight_Air',
+            'BroadPhaseofFlight_Ground','BroadPhaseofFlight_Landing','BroadPhaseofFlight_Takeoff',
+            'BroadPhaseofFlight_other/unknown','eng_type_REC','eng_type_TF','eng_type_TP','eng_type_TS',
+            'eng_type_other/unknown','far_part_091','far_part_121','far_part_135','far_part_137','far_part_PUBU',
+            'far_part_other/unknown','acft_make_beech','acft_make_bell','acft_make_boeing','acft_make_cessna',
+            'acft_make_mooney','acft_make_other/unknown','acft_make_piper','acft_make_robinson helicopter',
+            'acft_category_AIR','acft_category_HELI','acft_category_other/unknown','homebuilt_N','homebuilt_Y',
+            'fixed_retractable_FIXD','fixed_retractable_RETR',
+            'second_pilot_N','second_pilot_Y','second_pilot_other/unknown']
+
+X_train = train[features]
+y_train = train[target] 
+
+X_test = test[features]
+y_test = test[target]
+
+# label_map = {'MINR': 0, 'SUBS': 1, 'DEST': 2}
+y_train_encoded = y_train.map(label_map)
+y_test_encoded = y_test.map(label_map)
+
+hyper_params = ast.literal_eval(best_row['hyperparams'].values[0]) # best parameters found in part a for the best classifier
+
+if learner == "randomforest":
+    model = RandomForestClassifier( **hyper_params,random_state=seed)
+elif learner == "histgrad":
+    model = HistGradientBoostingClassifier( **hyper_params,random_state=seed)
+elif learner == "extrees":
+    model = ExtraTreesClassifier(**hyper_params,random_state=seed)
+else:
+    model = XGBClassifier(**hyper_params,random_state=seed)
+
+
+model.fit(X_train, y_train_encoded)
+
+############################################################
+##    Get Performance, confusion matrix,                  ##
+##    classification record, and feature importance       ##
+############################################################
+
+y_train_pred = model.predict(X_train)
+y_test_pred = model.predict(X_test)
+
+
+f1_train = f1_score(y_train_encoded, y_train_pred, average='macro')
+f1_test = f1_score(y_test_encoded, y_test_pred, average='macro')
+
+report_str = classification_report(y_test_encoded, y_test_pred)
+
+cm = confusion_matrix(y_test_encoded, y_test_pred)
+
+feature_names = X_train.columns
+# Feature importance DataFrame
+marker = False
+if hasattr(model, 'feature_importances_'):
+    # RandomForest, ExtraTrees, HistGradientBoosting
+    importances = model.feature_importances_
+    marker = True
+elif hasattr(model, 'get_booster'):
+    # XGBClassifier
+    booster = model.get_booster()
+    score_dict = booster.get_score(importance_type='gain')
+    
+    # Convert from dict to aligned list with all features
+    importances = [score_dict.get(f'f{i}', 0) for i in range(len(feature_names))]
+
+    marker = True
+
+## The following new_feature_importance_df should be the same as the feature_importance_df
+if marker:
+    new_feature_importance_df = pd.DataFrame({
+        'Feature': feature_names,
+        'Importance': importances
+    }).sort_values(by='Importance', ascending=False)
+
+
+    # top 10 important features
+    plt.figure(figsize=(10, 6))
+    sns.barplot(data=new_feature_importance_df.head(10), x='Importance', y='Feature', palette='Blues_r')
+    plt.title(f"Top 10 Feature Importances ({learner}, reproduced)")
+    plt.xlabel("Importance")
+    plt.ylabel("Feature")
+    plt.tight_layout()
+    plt.savefig("img/classification_feature_importance_reproduced.png", dpi=300)
+    plt.show()
+    plt.close()
+    
+# Classification Report 
+print("\nClassification Report on Testing dataset:")
+print(report_str)
+
+## Confusion Matrix
+labels_map = {v: k for k, v in label_map.items()}
+labels = [labels_map[i] for i in sorted(labels_map)]
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+disp.plot(cmap='Blues', xticks_rotation=45)
+disp.ax_.set_title("Confusion Matrix for testing dataset")
+disp.ax_.set_xlabel("Predicted Label")
+disp.ax_.set_ylabel("True Label")
+disp.figure_.tight_layout()
+plt.savefig('img/classification_confusion_matrix_test.png', dpi=300, bbox_inches='tight')  # Save as PNG
